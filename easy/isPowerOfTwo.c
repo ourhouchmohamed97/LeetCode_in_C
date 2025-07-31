e1b10370a97902da4ab4196bb16af391d181e88d
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-07-31 20:45:49 +0200

    feat: solve easy leetcode problem

---
+++ easy/isPowerOfTwo.c
@@ -0,0 +1,24 @@
+// Power of Two
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0) {
+        return false;
+    }
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int test_numbers[] = {0, 1, 2, 3, 4, 5, 8, 16, 17, 32, 33, -1, -2, -4};
+    int num_tests = sizeof(test_numbers) / sizeof(test_numbers[0]);
+    
+    for (int i = 0; i < num_tests; i++) {
+        int n = test_numbers[i];
+        bool result = isPowerOfTwo(n);
+        printf("%d is a power of two? %s\n", n, result ? "true" : "false");
+    }
+    
+    return 0;
+}
