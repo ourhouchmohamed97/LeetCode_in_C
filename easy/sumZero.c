945566222ab9d506537e84b000d7f63e4d7cdbc7
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-09-05 11:50:09 +0200

    feat: solve easy leetcode problem

---
+++ easy/sumZero.c
@@ -0,0 +1,34 @@
+// Find N Unique Integers Sum up to Zero
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int* sumZero(int n, int* returnSize) {
+    *returnSize = n;
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        arr[i] = 2 * i - n + 1;
+    }
+    return arr;
+}
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    
+    int returnSize;
+    int* result = sumZero(n, &returnSize);
+    
+    printf("Output: [");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d", result[i]);
+        if (i < returnSize - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    
+    free(result);
+    return 0;
+}
