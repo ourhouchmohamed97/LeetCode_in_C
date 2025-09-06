4d8e36458bcc49a121e080c93c7d3544446ecf01
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-09-06 11:32:07 +0200

    feat: solve easy leetcode problem

---
+++ easy/numberOfSteps.c
@@ -0,0 +1,29 @@
+// Number of Steps to Reduce a Number to Zero
+
+#include <stdio.h>
+
+int numberOfSteps(int num) {
+    int steps = 0;
+    while (num > 0) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+        }
+        steps++;
+    }
+    return steps;
+}
+
+int main() {
+    int num1 = 14;
+    printf("%d\n", numberOfSteps(num1));
+
+    int num2 = 8;
+    printf("%d\n", numberOfSteps(num2));
+
+    int num3 = 123;
+    printf("%d\n", numberOfSteps(num3));
+
+    return 0;
+}
