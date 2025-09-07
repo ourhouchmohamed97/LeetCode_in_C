cb2cabab1ac0b5dbafbe00963c803c46436ab3c6
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-09-07 12:21:19 +0200

    feat: solve easy prob

--- easy/numberOfSteps.c
+++ easy/numberOfSteps.c
@@ -4,12 +4,12 @@
 
 int numberOfSteps(int num) {
     int steps = 0;
-    while (num > 0) {
-        if (num % 2 == 0) {
+    while (num > 0)
+    {
+        if (num % 2 == 0)
             num /= 2;
-        } else {
+        else
             num -= 1;
-        }
         steps++;
     }
     return steps;
