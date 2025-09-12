db694bb898a0915fca3a5048b1bf1a61cbcccb57
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-09-12 12:37:20 +0200

    feat: solve easy leetcode problem

---
+++ easy/numberOfMatches.c
@@ -0,0 +1,17 @@
+// Count of Matches in Tournament
+
+#include <stdio.h>
+
+int numberOfMatches(int n) {
+    return n - 1;
+}
+
+int main(void) {
+    int n1 = 7;
+    int n2 = 14;
+
+    printf("n = %d -> %d\n", n1, numberOfMatches(n1));
+    printf("n = %d -> %d\n", n2, numberOfMatches(n2));
+
+    return 0;
+}
