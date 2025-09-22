f91665be2060ed9b04dd17ea3bb3bffd12848f9b
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-09-22 11:54:44 +0100

    feat: solve medium leetcode problem

---
+++ med/getSum.c
@@ -0,0 +1,14 @@
+// Sum of Two Integers
+
+#include <stdio.h>
+
+int getSum(int a, int b) {
+    return a + b;
+}
+
+int main()
+{
+    printf("%d\n", getSum(5, 6));
+    printf("%d\n", getSum(2, 3));
+    printf("%d\n", getSum(9, 61));
+}
