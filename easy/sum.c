b9937d7a7d5835ccd377a63b20fdf35bcf2660fe
Mohamed-Ourhouch <ourhouchmohamed97@gmail.com>
2025-09-24 20:16:10 +0200

    feat: solve easy leetcode problem

---
+++ easy/sum.c
@@ -0,0 +1,15 @@
+// Add Two Integers
+
+#include <stdio.h>
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main()
+{
+    printf("%d\n", sum(12, 5));
+    printf("%d\n", sum(5, 55));
+    printf("%d\n", sum(2, 11));
+    printf("%d\n", sum(4, 1));
+}
