f478594b2a264c5728598552289458da2ec77d3b
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-10-25 08:28:10 +0100

    feat: submit the streak

--- easy/totalMoney.c
+++ easy/totalMoney.c
@@ -3,19 +3,17 @@
 #include <stdio.h>
 
 int totalMoney(int n) {
-    if (n <= 0) return 0;
-
-    int weeks = n / 7;
-    int rem = n % 7;
-
-    long long total = 0;
-
-    total += (long long)weeks * 28;
-    total += 7LL * weeks * (weeks - 1) / 2;
-
-    total += (long long)rem * (1 + weeks) + (long long)rem * (rem - 1) / 2;
-
-    return (int)total;
+    int total = 0;
+    int weekStart = 1;
+    
+    for (int i = 0; i < n; i++) {
+        total += weekStart + (i % 7);
+        
+        if ((i + 1) % 7 == 0) {
+            weekStart++;
+        }
+    }
+    return total;
 }
 
 int main(void) {
