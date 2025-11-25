95aea0e143779aa18a2acf88a3abd2a5f340a51a
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-11-25 10:41:28 +0100

    feat: solve easy leetcode streak

---
+++ med/smallestRepunitDivByK.c
@@ -0,0 +1,11 @@
+// Smallest Integer Divisible by K
+
+int smallestRepunitDivByK(int k) {
+    int rem = 0;
+    for (int length = 1; length <= k; ++length) {
+        rem = (rem * 10 + 1) % k;
+        if (rem == 0)
+            return length;
+    }
+    return -1;
+}
