40aa2e317edf4325e7d9a5ab9e8a81f9c9e2004e
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-11-29 11:33:48 +0100

    feat: solve easy leetcode streak

---
+++ easy/minOperations.c
@@ -0,0 +1,8 @@
+// Minimum Operations to Make Array Sum Divisible by K
+
+int minOperations(int* nums, int numsSize, int k) {
+    int sum = 0;
+    for (int i = 0; i < numsSize; i++)
+        sum += nums[i];
+    return sum % k;
+}
