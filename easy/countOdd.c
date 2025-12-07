c15cf4d97b9564967fd8c6dd4ccb3420b13643d8
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-07 10:08:03 +0100

    feat: solve easy leetcode problem

---
+++ easy/countOdd.c
@@ -0,0 +1,11 @@
+// Count Odd Numbers in an Interval Range
+
+int countOdds(int low, int high){
+    int count = 0;
+    for(int i = low; i <= high; i++)
+    {
+        if(i % 2 != 0)
+            count++;
+    }
+    return count;
+}
