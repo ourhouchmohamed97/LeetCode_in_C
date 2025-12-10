f2a01e8fae008060ad0048239d19c3d5022142a8
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-12-10 10:21:57 +0100

    feat: solve medium leetcode streak

---
+++ med/countPermutations.c
@@ -0,0 +1,18 @@
+// Count the Number of Computer Unlocking Permutations
+
+#define MOD 1000000007
+
+int countPermutations(int* complexity, int complexitySize) {
+    int first = complexity[0];
+
+    for (int i = 1; i < complexitySize; i++) {
+        if (complexity[i] <= first) return 0;
+    }
+
+    long long fact = 1;
+    for (int i = 2; i < complexitySize; i++) {
+        fact = (fact * i) % MOD;
+    }
+
+    return (int)fact;
+}
