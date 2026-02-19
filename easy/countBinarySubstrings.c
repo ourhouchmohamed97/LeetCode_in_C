389a080e6f3b34adb6def306c43750c76ecb2d19
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-02-19 14:07:15 +0000

    Solve easy leetcode streak

---
+++ easy/countBinarySubstrings.c
@@ -0,0 +1,15 @@
+// Count Binary Substrings
+
+#pragma GCC optimize("O3, unroll-loops")
+int countBinarySubstrings(char* s) {
+    int prev=0, cur=1, cnt=0;
+    for (int i=1; s[i]!='\0'; i++){
+        if(s[i]==s[i-1]) cur++;
+        else{
+            cnt+=fmin(cur, prev);
+            prev=cur;
+            cur=1;
+        }
+    }
+    return cnt+fmin(cur, prev);
+}
