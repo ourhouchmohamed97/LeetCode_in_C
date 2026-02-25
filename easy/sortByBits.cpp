c081da141e8bc724426aafc375538e56f3eafdaf
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-02-25 13:31:44 +0000

    Solve easy leetcode streak

---
+++ easy/sortByBits.cpp
@@ -0,0 +1,14 @@
+// Sort Integers by The Number of 1 Bits
+
+
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), 
+        [](unsigned x, unsigned y){
+            return popcount(x)==popcount(y)?x<y
+            : popcount(x)<popcount(y);
+        });
+        return arr;
+    }
+};
