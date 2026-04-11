97c25af09d5f1921408e31d39244aa45ab863fd5
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-11 11:50:03 +0100

    Solve medium leetcode streak

---
+++ med/minimumDistance.c
@@ -0,0 +1,31 @@
+// Minimum Distance Between Three Equal Elements II
+
+int minimumDistance(int* nums, int numsSize) {
+    int* last = (int*)malloc((numsSize + 1) * sizeof(int));
+    int* second = (int*)malloc((numsSize + 1) * sizeof(int));
+    
+    for (int i = 0; i <= numsSize; i++) {
+        last[i] = -1;
+        second[i] = -1;
+    }
+
+    int min_dist = INT_MAX;
+
+    for (int i = 0; i < numsSize; i++) {
+        int num = nums[i];
+        if (second[num] != -1) {
+            int dist = i - second[num];
+            if (dist < min_dist) {
+                min_dist = dist;
+            }
+        }
+        second[num] = last[num];
+        last[num] = i;
+    }
+
+    int result = (min_dist == INT_MAX) ? -1 : 2 * min_dist;
+    
+    free(last);
+    free(second);
+    return result;
+}
