b28e765366fa1e29397d2f6148e1c2c5a5461fd1
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-13 11:33:43 +0100

    Solve easy leetcode streak

---
+++ easy/getMinDistance.c
@@ -0,0 +1,16 @@
+// Minimum Distance to the Target Element
+
+int getMinDistance(int* nums, int numsSize, int target, int start) {
+    int min = INT_MAX;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == target) {
+            int dist = abs(start - i);
+            if (dist < min) {
+                min = dist;
+            }
+        }
+    }
+
+    return min;
+}
