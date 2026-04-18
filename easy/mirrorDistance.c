0fdcedf27dce8f774efc027af42eddab16008fc8
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-18 12:11:17 +0100

    Solve easyleetcode streak

---
+++ easy/mirrorDistance.c
@@ -0,0 +1,10 @@
+// Mirror Distance of an Integer
+
+#pragma optimize("O3, unroll-loops")
+int mirrorDistance(int n) {
+    long long rev=0;
+    for(int x=n; x; x/=10){
+        rev=10*rev+x%10;
+    }
+    return abs(rev-n);
+}
