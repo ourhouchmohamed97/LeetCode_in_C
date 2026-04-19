ff50f5cac6f13947ff7a4572432289cbd624c806
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-19 04:18:39 +0100

    Solve easyleetcode streak

---
+++ med/maxDistance.c
@@ -0,0 +1,17 @@
+//  Maximum Distance Between a Pair of Values
+
+int maxDistance(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int i = 0, j = 0, ans = 0, currDist;
+
+    while (i < nums1Size && j < nums2Size){
+        if (nums1[i] > nums2[j]){
+            i++;
+        } else {
+            currDist = j - i;
+            ans = ans >= currDist ? ans : currDist;
+            j++;
+        }
+    }
+
+    return ans;
+}
