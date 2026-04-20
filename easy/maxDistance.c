87598396ba1a1d4c332fb22fb6d8a2100259b983
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-20 10:08:55 +0100

    Solve easy leetcode streak

---
+++ easy/maxDistance.c
@@ -0,0 +1,13 @@
+// Two Furthest Houses With Different Colors
+
+#pragma optimize("O3, unroll-loops")
+int maxDistance(int* colors, int n) {
+    int c0=colors[0], cN=colors[n-1];
+    int lMax=0, rMax=0;
+    for(int i=0; i<n; i++){
+        int c=colors[i];
+        if (c0!=c) lMax=fmax(lMax, i);
+        if (cN!=c) rMax=fmax(rMax, n-1-i);
+    }
+    return fmax(lMax, rMax);
+}
