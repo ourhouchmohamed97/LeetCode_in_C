6eeef63f3ac3394e03c6ffd4ade12ac054683428
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-04-22 11:33:55 +0100

    Solve medium leetcode streak

---
+++ med/twoEditWords.c
@@ -0,0 +1,45 @@
+// Words Within Two Edits of Dictionary
+
+bool is_valid_query( char const *query, char const *dictionaryEntry )
+{
+    size_t len = strlen( query );
+
+    int numDifferences = 0;
+    for( size_t i = 0; i < len; ++i )
+    {
+        if( query[i] != dictionaryEntry[i] )
+        {
+            ++numDifferences;
+        }
+
+        if( numDifferences > 2 )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+char** twoEditWords( char **queries, int queriesSize, char **dictionary, int dictionarySize, int *returnSize )
+{
+    int numValid = 0;
+    static char *validEntries[100];
+
+    for( int iQuery = 0; iQuery < queriesSize; ++iQuery )
+    {
+        for( int iDicEntry = 0; iDicEntry < dictionarySize; ++iDicEntry )
+        {
+            if( is_valid_query( queries[iQuery], dictionary[iDicEntry] ) )
+            {
+                validEntries[numValid] = queries[iQuery];
+                ++numValid;
+
+                break;
+            }
+        }
+    }
+
+    *returnSize = numValid;
+    return validEntries;
+}
