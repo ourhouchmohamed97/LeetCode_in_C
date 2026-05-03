5cff1ef8f226946cb21ece37f8508524870727ff
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-05-03 10:08:32 +0100

    Solve easy leetcode streak

---
+++ easy/rotateString.c
@@ -0,0 +1,15 @@
+// Rotate String
+
+
+bool rotateString(char* s, char* goal) {
+    int n = strlen(s);
+    if (n != strlen(goal)) 
+        return false;
+    char* doubled = (char*)malloc(2 * n + 1);
+    strcpy(doubled, s);
+    strcat(doubled, s);
+    bool result = strstr(doubled, goal) != NULL;
+    free(doubled);
+
+    return result;
+}
