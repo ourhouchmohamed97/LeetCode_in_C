67da2f9aaff852accf2e961af7cdeb53726db96e
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-05-15 11:32:12 +0100

    Solve easy leetcode streak

---
+++ med/findMin.c
@@ -0,0 +1,15 @@
+// Find Minimum in Rotated Sorted Array
+
+int findMin(int* nums, int numsSize) {
+    int left = 0;
+    int right = numsSize - 1;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
