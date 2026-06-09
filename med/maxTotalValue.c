70db10520007ae723e0fe54efe8380c4f15f3140
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-06-09 16:04:17 +0100

    Solve medium leetcode problem

---
+++ med/maxTotalValue.c
@@ -0,0 +1,13 @@
+// Maximum Total Subarray Value I
+
+
+long long maxTotalValue(int* nums, int numsSize, int k) {
+    int mn = nums[0], mx = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] < mn)
+            mn = nums[i];
+        if (nums[i] > mx)
+            mx = nums[i];
+    }
+    return 1LL * (mx - mn) * k;
+}
