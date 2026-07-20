a93567389dcbfc299e519ba7104e801900200423
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-07-20 11:01:16 +0100

    Solve easy leetcode problem

--- easy/findGcd.c
+++ easy/findGcd.c
@@ -1,8 +1,9 @@
-// Find GCD 
+// Find GCD
 
 int findGCD(int* nums, int numsSize) {
     int minNum = nums[0];
     int maxNum = nums[0];
+
     for (int i = 1; i < numsSize; i++) {
         if (nums[i] < minNum) {
             minNum = nums[i];
