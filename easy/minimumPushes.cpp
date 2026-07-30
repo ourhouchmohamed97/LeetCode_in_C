5f68d165b7be16caaa4bfd8ac1ea93c1e0a149b1
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2026-07-30 10:35:21 +0100

    Solve easy leetcode problem

---
+++ easy/minimumPushes.cpp
@@ -0,0 +1,11 @@
+// Minimum Number of Pushes to Type Word I
+
+#include <string>
+
+class Solution {
+public:
+    int minimumPushes(string A) {
+        auto q = A.size() >> 3, r = A.size() & 7;
+        return ((q << 2) + r) * (q + 1);
+    }
+};
